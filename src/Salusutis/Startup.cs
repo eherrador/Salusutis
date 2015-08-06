9218c9f265a98b8b42dca01c12346f027b8d228e
Edgar Herrador <eherrador@icloud.com>
2015-08-05 19:10:37 -0500

    Enabling Facebook Authentication

--- src/Salusutis/Startup.cs
+++ src/Salusutis/Startup.cs
@@ -131,7 +131,7 @@ namespace Salusutis
 
             // Add authentication middleware to the request pipeline. You can configure options such as Id and Secret in the ConfigureServices method.
             // For more information see http://go.microsoft.com/fwlink/?LinkID=532715
-            // app.UseFacebookAuthentication();
+            app.UseFacebookAuthentication();
             // app.UseGoogleAuthentication();
             app.UseMicrosoftAccountAuthentication();
             // app.UseTwitterAuthentication();
